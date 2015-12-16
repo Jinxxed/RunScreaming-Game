b2e96ca7dafabdcd6e8b500e2345b97ecae34dae
Anja Karl <anja.karl@student.tugraz.at>
2015-12-16 10:40:47 +0100

    renamed some variables and fixed a debug message

--- Run Screaming/Assets/Scripts/Maze.cs
+++ Run Screaming/Assets/Scripts/Maze.cs
@@ -53,13 +53,13 @@ public class Maze : MonoBehaviour
 
         start = this.GetCell(this.RandomCoordinates);
 
-        LinkedList<MazeCell> pathes = new LinkedList<MazeCell>();
-        MazeCell goal = calculatePaths(start, pathes);
+        LinkedList<MazeCell> path = new LinkedList<MazeCell>();
+        MazeCell goal = calculatePaths(start, path);
         while(goal == null)
         {
-            pathes.Clear();
+            path.Clear();
             start = this.GetCell(this.RandomCoordinates);
-            goal = calculatePaths(start, pathes);
+            goal = calculatePaths(start, path);
         }
 
         FirstPersonController fpc = Instantiate(fpcPrefab) as FirstPersonController;
@@ -75,24 +75,24 @@ public class Maze : MonoBehaviour
         activeCells.Add(CreateCell(RandomCoordinates));
     }
 
-    public MazeCell calculatePaths(MazeCell activeCell, LinkedList<MazeCell> markedCells)
+    public MazeCell calculatePaths(MazeCell activeCell, LinkedList<MazeCell> path)
     {
-        if (markedCells.Count > 5 * size.x)
+        if (path.Count > 5 * size.x)
         {
             Debug.Log("Goal Calculated!");
-            Debug.Log("calculatePaths contains " + markedCells.Count + 1 + " tiles");
+            Debug.Log("Calculated path contains " + (path.Count + 1) + " tiles");
             return activeCell;
         }
 
-        if (markedCells.Contains(activeCell))
+        if (path.Contains(activeCell))
             return null;
-        markedCells.AddLast(activeCell);
+        path.AddLast(activeCell);
 
         for (int i = 0; i < MazeDirections.Count; i++)
             if (!(activeCell.GetEdge((MazeDirection)i) is MazeWall))
             {
-                LinkedList<MazeCell> path = new LinkedList<MazeCell>(markedCells);
-                MazeCell goal = calculatePaths(activeCell.GetEdge((MazeDirection)i).otherCell, path);
+                LinkedList<MazeCell> newPath = new LinkedList<MazeCell>(path);
+                MazeCell goal = calculatePaths(activeCell.GetEdge((MazeDirection)i).otherCell, newPath);
                 if (goal != null)
                 {
                     return goal;
