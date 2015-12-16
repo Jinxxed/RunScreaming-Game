d6ac80242d4def6b429ad83a376165371c9d0ae8
Anja Karl <anja.karl@student.tugraz.at>
2015-12-16 22:45:50 +0100

    added Creation of PNG with Maze after Generation

--- Run Screaming/Assets/Scripts/GameManager.cs
+++ Run Screaming/Assets/Scripts/GameManager.cs
@@ -1,58 +1,91 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityStandardAssets.Characters.FirstPerson;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
-	public Maze mazePrefab;
+    public Maze mazePrefab;
     public Camera mainCamera;
 
-	private Maze mazeInstance;
+    private Maze mazeInstance;
 
-	private void Start () {
-		BeginGame();
-	}
-	
-	private void Update () {
-		if (Input.GetKeyDown(KeyCode.Escape)) {
-			RestartGame();
-		}
-	}
+    private void takeScreenshot()
+    {
+        int width = (mazeInstance.size.x + 20) * 20;
+        int height = (mazeInstance.size.z + 20) * 20;
+
+        mainCamera.pixelRect = new Rect(0, 0, width, height);
+        RenderTexture rt = new RenderTexture(width, height, 24);
+
+        mainCamera.targetTexture = rt;
+        mainCamera.Render();
+        mainCamera.targetTexture = null;
+
+        RenderTexture.active = rt;
+
+        Texture2D screen = new Texture2D(width, height);
+        screen.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+
+        byte[] bytes = screen.EncodeToPNG();
+
+        string filename = Application.dataPath + "/MazeScreen.png";
+        System.IO.File.WriteAllBytes(filename, bytes);
+
+        Debug.Log("Created Maze Plot, saved it to " + filename);
+    }
+
+    private void Start()
+    {
+        BeginGame();
+        
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            RestartGame();
+        }
+    }
 
     public void switchCamera()
     {
         if (mazeInstance == null)
         {
             throw new System.Exception("Maze is null!");
-            
+
         }
         GameObject fps = FindObjectOfType<FirstPersonController>().gameObject;
         if (fps == null)
         {
             throw new System.Exception("Camera not found!");
         }
-        if(mainCamera.enabled)
+        if (mainCamera.enabled)
         {
-            fps.GetComponentInChildren<Camera>().enabled = true;
             mainCamera.enabled = false;
-            mainCamera.gameObject.SetActive(false);
-
+            fps.GetComponentInChildren<Camera>().enabled = true;
         }
         else
         {
+            fps.GetComponentInChildren<Camera>().enabled = false;
             mainCamera.enabled = true;
             mainCamera.gameObject.SetActive(true);
-            fps.GetComponentInChildren<Camera>().enabled = false;
         }
-        
+
     }
 
-	private void BeginGame () {
-		mazeInstance = Instantiate(mazePrefab) as Maze;
-		mazeInstance.Generate();
-	}
+    private void BeginGame()
+    {
+        mazeInstance = Instantiate(mazePrefab) as Maze;
+        mazeInstance.Generate();
+        takeScreenshot();
+    }
 
-	private void RestartGame () {
-		Destroy(mazeInstance.gameObject);
-		BeginGame();
-	}
+    private void RestartGame()
+    {
+        Destroy(mazeInstance.gameObject);
+        BeginGame();
+    }
 }
