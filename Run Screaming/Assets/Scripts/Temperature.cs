effcf139622c105213adee3643739e5a82c8f1a4
Roland <urbano.roland@gmail.com>
2016-01-11 22:22:02 +0100

    new images, updated countdown script

---
+++ Run Screaming/Assets/Scripts/Temperature.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Temperature : MonoBehaviour
+{
+    float timer = 0;
+    float end = 180;
+    int temperature = 7;
+    int threashold = 10;
+
+    void Start()
+    {
+        
+    }
+ 
+    void Update()
+    {
+        timer += Time.deltaTime;
+    }
+
+    void OnGUI()
+    {
+        if (timer >= end)
+        {
+            SceneManager.LoadScene("lose");
+        }
+        else
+        {
+            int newTemperature = temperature + ((int)timer / threashold);
+            GUI.Label(new Rect(20, 20, 200, 100),
+                                    "Beer temperature: " + newTemperature);
+        }
+    }
+}
